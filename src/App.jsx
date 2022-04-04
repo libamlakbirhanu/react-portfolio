112a363c760b6aacbf579260937103fc9e2dc9d5
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-04-04 16:49:47 +0300

    ADD: about page and it's style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import './App.scss'
+import About from './pages/About'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
         </Route>
       </Routes>
     </>
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,1 +1,26 @@
+import AnimatedLetters from '../../components/AnimatedLetters'
 import './index.scss'
+
+function About() {
+  return (
+    <div className='container about-page'>
+        <div className="text-zone">
+            <h1><AnimatedLetters letters={['a','b','o','u','t', ' ','m','e']} index={15} /></h1>
+            <p>I'm very ambitious front-end developer looking for a role in
+                established IT company with the opportunity to work with the latest
+                technologies on challenging and diverse projects.</p>
+            <p>
+                I'm quietly confident, naturally curious, and perpetually working on
+                improving my chops one design problem at a time.
+                </p>
+            <p>
+                If I need to define myself in one sentence, that would be a family
+                person, dear friend to a lot of people, a sports fanatic, gaming
+                enthusiast, and tech-obsessed!!!
+                </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
