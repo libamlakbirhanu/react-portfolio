08e9ac6996d397c162cc8d0f04aa2834143c7222
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-10-13 15:19:41 +0300

    changed the main color and added contact and portfolio page routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import './App.scss'
 import About from './pages/About'
-
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
